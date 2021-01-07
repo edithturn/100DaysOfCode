1efef3629c94523df1a4029d55c829e1f90cdb5d
Edi <edithpuclla20@gmail.com>
2021-01-06 23:16:39 -0500

    day001

---
+++ 001/Readme.md
@@ -0,0 +1,3 @@
+# Day 001 -  Beginner - Working with Variables in Python to Manage Data
+
+- Printing in the console in Python
---
+++ 001/day001-printing-start.py
@@ -0,0 +1,56 @@
+# Priting
+print("Day 001 - Python Print Function")
+print("The function is declared like this:")
+print('print("what to print")')
+
+# String Manipulation
+print("Hello world!\nHello world")
+print("Hello" + " " + "Edith")
+
+# Fixing the code below, the first line is printing the + sign
+print("Day 1 - String Manipulation" + ' "+" ' + "sign")
+print('Day 1 - String Manipulation "+" sign')
+print('e.g print("Hello" + "World")')
+
+# Input function , the first funciton input is going to execute first
+input("What is your name?")
+print("Hello " + input("What is your name?"))
+
+# Function len in python to know the number of characters of an string
+print(len(input("What is your name? ")))
+
+# Python variables
+name = input("What is your name? ")
+print(name)
+
+# Changing Variables | this will print first Edith then Tomas, bcz we are changing the value of the variable
+name = "Edith"
+print(name)
+
+name = "Tomas"
+print(name)
+
+# Simplifying code using variables
+name = input("What is your name? ")
+print(len(name))
+
+
+# -- Exercise Variables --
+a = input("a:")
+b = input("b:")
+
+# Swap the values of the variables to print a different result of the input
+c = a
+a = b
+b = c
+
+print("a = " + a)
+print("b = " + b)
+
+# Naming Varaibles: Itis important asign good names to the variable to be readable  for other people or for us in the future
+first_name = "Edith"
+last_name = "Ohio"
+print(first_name)
+print(last_name)
+
+print("hola"+"fasfa")
