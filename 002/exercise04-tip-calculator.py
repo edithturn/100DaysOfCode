618cbcaa7ce4aa0eff6eda77df077333b04ac59f
Edi <edithpuclla20@gmail.com>
2021-01-08 00:29:06 -0500

    day002 exer04

---
+++ 002/exercise04-tip-calculator.py
@@ -0,0 +1,13 @@
+print("welcomer to the tip calculator.")
+
+total_bill = input("What was the tital bill? ")
+percentage_bill = input("What percentage tip would you like to give? 10 , 12, or 15?")
+qantity_people = input("How many people split the bill? ")
+
+
+new_percentage_bill = ((int(percentage_bill) / 100) * float(total_bill))  + float(total_bill)
+bill_per_person =  new_percentage_bill / int(qantity_people)
+
+print(f"Each person shou; pay: {round(bill_per_person, 2)}")
+
+
