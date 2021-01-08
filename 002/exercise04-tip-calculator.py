ad09db947dacaf4e30eb230702452a7238907a48
Edi <edithpuclla20@gmail.com>
2021-01-08 00:39:05 -0500

    day002 done

--- 002/exercise04-tip-calculator.py
+++ 002/exercise04-tip-calculator.py
@@ -7,7 +7,8 @@ qantity_people = input("How many people split the bill? ")
 
 new_percentage_bill = ((int(percentage_bill) / 100) * float(total_bill))  + float(total_bill)
 bill_per_person =  new_percentage_bill / int(qantity_people)
-
-print(f"Each person shou; pay: {round(bill_per_person, 2)}")
+final_amount = round(bill_per_person, 2)
+final_amount = "{:.2f}".format(bill_per_person)
+print(f"Each person shou; pay: ${final_amount}")
 
 
