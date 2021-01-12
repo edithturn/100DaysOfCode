595a370d9b038fdd470948575f3216b84479f89e
Edi <edithpuclla20@gmail.com>
2021-01-11 19:52:15 -0500

    describing code

--- 003/exercise-09-love-calculator.py
+++ 003/exercise-09-love-calculator.py
@@ -2,23 +2,30 @@ print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 
+# Applying lower case for both text and concatenating
 both = name1.lower() + " " + name2.lower()
 
+# Defining the words to compare with the names
 true = "true"
 love = "love"
 
+# Initializaing the counters
 count_true = 0
 count_love = 0
 
-
+# Searching each letter of the word "true" in both names
 for i in range(len(true)):
+# Counting the number of letters mached
     count_true += both.count(true[i])
+# Searching each letter of the word "love" in both names
 for j in range(len(love)):
+# Counting the number of letters mached
     count_love += both.count(love[j])
-print(count_true)
-print(count_love)
 
+# Concatenating both numbers in a single digit
 score = str(count_true) + str(count_love)
+
+# Changing data type  from string to integer
 _score = int(score)
 
 if _score < 10 or _score > 90:
