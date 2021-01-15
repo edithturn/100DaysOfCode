19104994c3c9bad9499917a500db91fa45f6ffc6
Edi <edithpuclla20@gmail.com>
2021-01-14 20:28:57 -0500

    finishe psr

--- 004/exercise-04-rock-paper-scissors.py
+++ 004/exercise-04-rock-paper-scissors.py
@@ -29,32 +29,40 @@ scissors = '''
 
 print("====== Rock Paper Scissors ======")
 
+# Asking for input to the user
 choose = int(input("\nType [ 1 ] for rockr , [ 2 ] for paper, [ 3 ] for scissors \n"))
 
 print("\nWhat I chose")
+# Assigning a integer value to a each simbol of the play  [sock = 1 , paper = 2 and scissors = 3], in case user insert a invalid number show a menssage
 if choose == 1:
     print(rock)
 elif choose == 2:
     print(paper)
-else:
+elif choose == 3:
     print(scissors)
-
-print("Computer chose")
-computer_choose = random.randint(1, 3)
-
-if computer_choose == 1:
-    print(rock)
-elif computer_choose == 2:
-    print(paper)
 else:
-    print(scissors)
+    print("Invalid Number! Type a number 1, 2 or 3")
 
-if (choose == 1 and computer_choose == 3) or (choose == 3 and computer_choose == 2) or (choose == 2 and computer_choose == 1):
-    print ("You win!")
-elif (choose == computer_choose):
-    print("Play Again? [ Yes ] [ Not ]")
-else:
-    print("You Lose!")
+# If user choose valid number, then generate a random image for the computer
+if choose >= 1 and choose <= 3:
+    print("Computer chose")
+    computer_choose = random.randint(1, 3)
+
+    if computer_choose == 1:
+        print(rock)
+    elif computer_choose == 2:
+        print(paper)
+    else:
+        print(scissors)
+
+# Applying the rules of the game
+
+    if (choose == 1 and computer_choose == 3) or (choose == 3 and computer_choose == 2) or (choose == 2 and computer_choose == 1):
+        print ("You win!")
+    elif (choose == computer_choose):
+        print("Play Again? [ Yes ] [ Not ]")
+    else:
+        print("You Lose!")
 
 
 # 1 rock win scissors 3
