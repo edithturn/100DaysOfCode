f5fcdde97e2ab0e050233099cbf1eabe1b790c77
Edi <edithpuclla20@gmail.com>
2021-01-16 22:08:29 -0500

    another wy to calculate the highest score

--- 005/exercise-02-max-score.py
+++ 005/exercise-02-max-score.py
@@ -5,13 +5,22 @@ for n in range(0, len(student_scores)):
 # Defining the first value for the variable max_score
 max_score = student_scores[0]
 
+# First Way:
 # Iterating all the elements of the studen_score, iterating by index
 for n in range(1, len(student_scores)):
 # Converting to int data type to do calculations
     student_scores[n] = int(student_scores[n])
 # Comparing the next element in student_scores with the first value
-    if student_scores[n] > max_score:
 # If the next element is grather than max_score, then max_score will take the new value.
+    if student_scores[n] > max_score:
         max_score = student_scores[n]
 
-print(max_score)
+print(f"The highest score in the class is {max_score}")
+
+# Second Way
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+
+print(f"The highest score in the class is {highest_score}")
