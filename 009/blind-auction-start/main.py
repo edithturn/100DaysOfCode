830c0f17bc3182fdd1141738b8b1bb0c6ec24de4
Edi <edithpuclla20@gmail.com>
2021-02-13 23:54:45 -0500

    fx max amount

--- 009/blind-auction-start/main.py
+++ 009/blind-auction-start/main.py
@@ -10,15 +10,24 @@ auction_dic = {}
 def fill_dic(name, amount):
     auction_dic[name] = amount
 
+def finding_max(auction_dic):
+    highest_bid = 0
+    winner = ""
+    for n in auction_dic:
+        bid_amount = auction_dic[n]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = n
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
 while not option:
     name = input("Name:")
-    amount = int(input("What is your bid?"))
+    amount = int(input("What is your bid? \n"))
     fill_dic(name, amount)
-    selecction = input("Do yo have more? yes or no \n")
+    selecction = input("Do yo have more? [yes] or [no] \n")
     if selecction == "no":
         option = True
     os.system('clear')
 
-auction_max = max(auction_dic.values())
+finding_max(auction_dic)
 print(auction_dic)
-print(auction_max)
