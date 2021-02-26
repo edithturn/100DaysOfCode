064783afc1c9e0c00ff2b9793d1ace6191caa2e2
Edi <edithpuclla20@gmail.com>
2021-02-26 18:02:48 -0500

    fixing the bug

--- 010/exercise-04-calculator.py
+++ 010/exercise-04-calculator.py
@@ -22,10 +22,18 @@ num1 = int(input("What is the first number? "))
 for key in operations:
     print(key)
 
-operation_symbol = input("Pick an operation from the line above:")
-num2 = int(input("What is the second number?"))
+operation_symbol = input("Pick an operation from the line above:\n")
+num2 = int(input("What is the second number?\n"))
 
+# Using the parameter operation_symbol to search into the dictionary 
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num2, num2)
+first_answer = calculation_function(num1, num2)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+operation_symbol1 = input("Pick another operation:\n")
+num3 = int(input("what's the next number? \n"))
+calculation_function1 = operations[operation_symbol1]
+second_answer = calculation_function1( calculation_function(num1, num2), num3)
+
+print(f"{first_answer} {operation_symbol1} {num3} = {second_answer}")
