fabb5ee83177e64c04380d376e4296f6ec8e5af7
Edi <edithpuclla20@gmail.com>
2021-02-26 17:27:35 -0500

    calc

--- 010/exercise-04-calculator.py
+++ 010/exercise-04-calculator.py
@@ -7,16 +7,25 @@ def sub(a, b):
 def mult(a, b):
     return a * b
 
-def div(a, b)
+def div(a, b):
     return a / b
 
-operations = 
-{
+operations = {
 "+": add,
 "-": sub,
 "*": mult, 
 "/": div
 }
 
-function = operations["*"]
-function(2, 3)
+num1 = int(input("What is the first number? "))
+
+for key in operations:
+    print(key)
+
+operation_symbol = input("Pick an operation from the line above:")
+num2 = int(input("What is the second number?"))
+
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num2, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
