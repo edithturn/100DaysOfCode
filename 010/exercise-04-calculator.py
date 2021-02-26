c21281e854cf3cf00520088eb9eb4ef5453cba4d
Edi <edithpuclla20@gmail.com>
2021-02-26 18:45:21 -0500

    calculator done

--- 010/exercise-04-calculator.py
+++ 010/exercise-04-calculator.py
@@ -22,8 +22,8 @@ num1 = int(input("What is the first number? "))
 for key in operations:
     print(key)
 
-operation_symbol = input("Pick an operation from the line above:\n")
-num2 = int(input("What is the second number?\n"))
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int(input("What is the second number? "))
 
 # Using the parameter operation_symbol to search into the dictionary 
 calculation_function = operations[operation_symbol]
@@ -31,9 +31,19 @@ first_answer = calculation_function(num1, num2)
 
 print(f"{num1} {operation_symbol} {num2} = {first_answer}")
 
-operation_symbol1 = input("Pick another operation:\n")
-num3 = int(input("what's the next number? \n"))
-calculation_function1 = operations[operation_symbol1]
-second_answer = calculation_function1( calculation_function(num1, num2), num3)
+keep_calculating = False
 
-print(f"{first_answer} {operation_symbol1} {num3} = {second_answer}")
+while keep_calculating == False:    
+    option = input(f"Type 'y' to continue calculating with {first_answer}, or type 'n' to exit.: ")
+    if min(option) == 'y':
+        operation_symbol1 = input("Pick another operation: ")
+        num3 = int(input("what's the next number? "))
+        calculation_function1 = operations[operation_symbol1]
+        second_answer = calculation_function1(first_answer, num3)
+        print(f"{first_answer} {operation_symbol1} {num3} = {second_answer}")
+        first_answer = second_answer
+    elif min(option) == 'n':
+        keep_calculating == True
+        break
+    else:
+        print("You didn't type any value!")
