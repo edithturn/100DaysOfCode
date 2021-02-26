cb1047c56ddae1f1b101c557cce0a7f2cd441dd8
Edi <edithpuclla20@gmail.com>
2021-02-25 22:08:24 -0500

    returned value

--- 010/exercise-02-start.py
+++ 010/exercise-02-start.py
@@ -1,7 +1,7 @@
 def format_name(f_name, l_name):
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
-    print(f"{formated_f_name} {formated_l_name}")
+    return formated_f_name + " " + formated_l_name
 
 name1 = "edith"
 last_name1 = "puclla"
@@ -9,5 +9,5 @@ last_name1 = "puclla"
 name2 = "EDiTh"
 last_name2 = "PUCLLA"
 
-format_name(f_name = name1, l_name = last_name2)
-format_name(f_name = name2, l_name = last_name2)
+print(format_name(f_name = name1, l_name = last_name2))
+print(format_name(f_name = name2, l_name = last_name2))
