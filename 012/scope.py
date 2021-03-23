fe253bbbd8dd6dce7b42bb4c7e281bfad1344a95
Edi <edithpuclla20@gmail.com>
2021-03-22 23:15:24 -0500

    score of variabel

--- 012/scope.py
+++ 012/scope.py
@@ -28,3 +28,40 @@ def game():
 
     drink_potion()
 print(f"Outside the function: {player_health}")
+
+
+
+game_level = 3
+def create_enemy():
+    enemies = ["Skeleton", "Zombie", "Alien"]
+    if game_level < 4:
+        new_enemy = enemies[0]
+
+    print(new_enemy)
+
+create_enemy()
+
+# Modifying Global Scope
+
+soldiers = "Skeleton"
+
+def increaseEnemies():
+    soldiers = "Zombie"
+    print(f"soldiers inside function: {soldiers}")
+
+increaseEnemies()
+print(f"soldiers inside function: {soldiers}")
+
+
+
+# Modifying Global Scope
+
+soldier = 1
+
+def increaseSoldiers():
+    global soldier
+    soldier += 2
+    print(f"Soldier inside function: {soldier}")
+
+increaseSoldiers()
+print(f"Soldier inside function: {soldier}")
