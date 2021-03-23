e1d71565225c00a3632ad08ea9a531df0f3369be
Edi <edithpuclla20@gmail.com>
2021-03-23 01:32:24 -0500

    done

--- 012/art.py
+++ 012/art.py
@@ -11,21 +11,49 @@ logo = """
  """
 print("Welcome to the Number Guessing Game!")
 print("I am thinking a number betweek 1 and 100")
-def thinkRandomNumber():
-    num = random.randint(1, 100)
-    return num
 
-keep_asking = True
+# variables
+num = random.randint(1, 100)
 
-while keep_asking:
-    difficulty = input(f"Choose a difficulty. Type 'easy' or 'hard' : ")
-    if difficulty.lower() == "easy":
-        attemps = 5
-        keep_asking = False
-    elif difficulty.lower() == "hard":
-        attemps = 10
-        keep_asking = False
+def Game():
+    keep_asking_attemps = True
+    attemps = 0
+    while keep_asking_attemps:
+        difficulty = input(f"Choose a difficulty. Type [ easy ] or [ hard ] : ")
+        if difficulty.lower() == "easy":
+            attemps = 5
+            keep_asking_attemps = False
+        elif difficulty.lower() == "hard":
+            attemps = 10
+            keep_asking_attemps = False
+    print(f"You have {attemps} attempts remaining to guess the number")
+    return attemps
 
+def Guessing():
+    keep_guessing = True
+    attemp = Game()
+    print(f"Number {num}")
+       
+    while keep_guessing:
+        guess_number = int(input(f"Make a guess : "))
+       
+        if guess_number > num:
+            print("Too High")
+            attemp -= 1
+            keep_guessing = True
+        elif guess_number < num:
+            print("Too Low")
+            attemp -= 1
+            keep_guessing = True
+        elif guess_number == num:
+            print(f"You got it! the answer was {num}")
+            return 
+        else:
+            print("It is not a number")
+            keep_guessing = True
+        if attemp == 0:
+            print("You lost your attemps, now is {attemp}!")
+            return
+        print(f"you have {attemp} attemps to guess the number")
 
-
-print(thinkRandomNumber())
+Guessing()
